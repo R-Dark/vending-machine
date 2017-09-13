9462839ece8121fbfccb50ea21ea1fe4d1f1c2ff
rob dark <robdark@robs-MacBook-Pro.local>
2017-09-13 18:22:10 -0500

    added purchase function

--- routes/purchase.js
+++ routes/purchase.js
@@ -3,20 +3,32 @@ const router = express.Router()
 const models = require("../models")
 
 router.post("/api/customer/items/:itemId/purchases", function(req, res) {
-  models.vendingItems
-    .find({
-      where: {
-        id: req.params.itemId
-      }
-    })
-    .then(function(purchase) {
-      item.quantity -= (1).item.save().then(function(purchase) {
-        res.json({ purchase: purchase })
-      })
-    })
-    .catch(function(error) {
-      res.status(404).json({ errorMessage: "ERROR" })
-    })
-})
+      let purchaseMoney = req.body.moneyGiven
+      models.vendingItem
+        .find({
+          where: {
+            id: req.params.itemId
+          }
+        })
+        .then(function(item) {
+            let change = purchaseMoney - item.cost
+            item.quantity -= (1).item.save().then(function(item) {
+                models.money.find().then(function(money) {
+                  if (change > 0) {
+                    money.totalmoney += item.cost
+                  }
+                })
+                money.save()
+                  .then(function(money) {
+                    res.json({
+                      item: item })
+                    })
+                  })
+              })
+              .catch(function(error) {
+                res.status(404).json({
+                  errorMessage: "ERROR" })
+                })
+              })
 
-module.exports = router
+            module.exports = router

9d9db00dd9e481799a86f30be51ecf8f9503ae84
rob dark <robdark@robs-MacBook-Pro.local>
2017-09-14 16:29:52 -0500

    added money log and purchase log posts and gets

--- routes/money.js
+++ routes/money.js
@@ -2,6 +2,11 @@ const express = require("express")
 const router = express.Router()
 const models = require("../models")
 
+router.get("/api/vendor/money/count", function(req, res){
+  models.moneycounter.findAll().then(function(moneycounter){
+    res.json(moneycounter)
+  })
+})
 
 router.get("/api/vendor/money", function(req, res) {
   models.money.findAll().then(function(money) {
@@ -15,6 +20,24 @@ router.post("/api/vendor/money", function(req, res) {
   })
   newMoney.save().then(function(money) {
     res.json({ success: true })
+  })
+})
+
+router.put('/api/vendor/money/:itemId', function (req, res) {
+  models.money.find({
+    where: {
+      id: req.params.itemId
+    }
+  })
+  .then(function (money) {
+    money.totalmoney = req.body.totalmoney
+    money.save()
+    .then(function (money) {
+      res.json({money: money})
+    })
+  })
+  .catch(function (error) {
+    res.status(404).json({errorMessage: 'Item not found'})
   })
 })
 
--- routes/purchase.js
+++ routes/purchase.js
@@ -2,33 +2,58 @@ const express = require("express")
 const router = express.Router()
 const models = require("../models")
 
-router.post("/api/customer/items/:itemId/purchases", function(req, res) {
-      let purchaseMoney = req.body.moneyGiven
-      models.vendingItem
-        .find({
-          where: {
-            id: req.params.itemId
-          }
-        })
-        .then(function(item) {
-            let change = purchaseMoney - item.cost
-            item.quantity -= (1).item.save().then(function(item) {
-                models.money.find().then(function(money) {
-                  if (change > 0) {
-                    money.totalmoney += item.cost
-                  }
-                })
-                money.save()
-                  .then(function(money) {
-                    res.json({
-                      item: item })
-                    })
-                  })
-              })
-              .catch(function(error) {
-                res.status(404).json({
-                  errorMessage: "ERROR" })
-                })
-              })
+router.get("/api/vendor/purchases", function(req, res){
+  models.log.findAll().then(function(purchases){
+    res.json(purchases)
+  })
+})
 
-            module.exports = router
+
+router.post("/api/customer/items/:itemId/purchases", function(req, res) {
+  let purchaseMoney = req.body.moneyGiven
+  models.vendingItem
+    .find({
+      where: {
+        id: req.params.itemId
+      }
+    })
+    .then(function(item) {
+      let change = purchaseMoney - item.cost
+      item.quantity -= (1)
+      item.save().then(function() {
+        models.money.findAll().then(function(money) {
+          const moneyRecord = money[0]
+          if (change >= 0) {
+            moneyRecord.totalmoney += item.cost
+          }
+          moneyRecord.save()
+          .then(function(){
+            let moneyCounter = models.moneycounter.build({
+              moneycount : moneyRecord.totalmoney
+            })
+            moneyCounter.save()
+          })
+          .then(function(){
+            let newLog = models.log.build({
+              name: item.name
+            })
+            newLog.save()
+          })
+            .then(function(money) {
+              res.json({
+                money: money
+              })
+            })
+        })
+      })
+    })
+    .catch(function(error) {
+      console.log(error);
+      res.status(404).json({
+        errorMessage: "ERROR"
+      })
+    })
+})
+
+
+module.exports = router
